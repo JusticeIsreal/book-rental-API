3e3b83e9112e554a055df590d1cf5ee42b0f0881
Justice Isreal <justiceyba@gmail.com>
2023-02-20 02:14:30 +0100

    admin view all register user list

--- Controller/Auth/usersCRUD.js
+++ Controller/Auth/usersCRUD.js
@@ -33,8 +33,6 @@ const allUsers = async (req, res) => {
 // update user position to give access
 const updateUserPosition = async (req, res) => {
   try {
-    const { id: userId } = req.body;
-
     const auth = req.headers.authorization;
     if (!auth || !auth.startsWith("Bearer ")) {
       return res.status(401).json({
@@ -52,20 +50,63 @@ const updateUserPosition = async (req, res) => {
     const allowAccess = await adminAccessSchema.findOne({
       _id: verifyToken.id,
     });
-    if (!allowAccess.verified === true || !allowAccess.position === "owner") {
+    if (allowAccess.verified != true || allowAccess.position != "owner") {
       return res.status(401).json({
         status: "ERROR",
         message: "You are not authorized to perform this action",
       });
     }
-    const user = await adminAccessSchema.findOneAndUpdate({ _id: userId });
-    if (!user) {
-      res.status(401).json({ status: "ERROR", message: "User not found" });
-    }
-
-    res.status(200).json({ user });
+    const user = await adminAccessSchema.findOneAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({ status: "SUCCESS", data: user });
   } catch (error) {
     throw new Error(error.message);
   }
 };
-module.exports = { allUsers, updateUserPosition };
+
+// DELETE USER
+const deleteUser = async (req, res) => {
+  try {
+    const auth = req.headers.authorization;
+
+    if (!auth || !auth.startsWith("Bearer ")) {
+      return res.status(401).json({
+        status: "FAILED",
+        message: "No token provided, You dont have access to this data",
+      });
+    }
+    const token = auth.split(" ")[1];
+    const verifyToken = await jwt.verify(token, process.env.SECRET);
+
+    if (!verifyToken) {
+      return res
+        .status(401)
+        .json({ status: "ERROR", message: "Invalide token access" });
+    }
+
+    const allowAccess = await adminAccessSchema.findOne({
+      _id: verifyToken.id,
+    });
+    if (allowAccess.verified != true || allowAccess.position != "owner") {
+      return res.status(401).json({
+        status: "ERROR",
+        message: "You are not authorized to perform this action",
+      });
+    }
+
+    const deleteUser = await adminAccessSchema.findOneAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ status: "USER DELETe SUCCESSFUL", data: deleteUser });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = { allUsers, updateUserPosition, deleteUser };
--- Route/userAuthRoute.js
+++ Route/userAuthRoute.js
@@ -8,6 +8,7 @@ const resendOTP = require("../Controller/Auth/resendOTP");
 const {
   allUsers,
   updateUserPosition,
+  deleteUser,
 } = require("../Controller/Auth/usersCRUD.js");
 
 router.post("/registeruser", registerUser);
@@ -15,6 +16,7 @@ router.post("/verifyotp", verifyRegOTP);
 router.post("/resendotp", resendOTP);
 router.post("/loginuser", loginUser);
 router.get("/allusers", allUsers);
-router.patch("/updateusers/:id", updateUserPosition);
+router.patch("/updateuser/:id", updateUserPosition);
+router.delete("/deleteuser/:id", deleteUser);
 
 module.exports = router;
